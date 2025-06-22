9eae053bd21f85e78503a1b7cf1060d7a4c2a9a4
Hizkia Jonathan Budiana <hizkia.jonathanb@gmail.com>
2025-06-22 12:15:40 +0700

    5th

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,36 +2,34 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-white shadow-sm sticky-top">
-      <div className="container">
+    <nav className="navbar bg-white shadow-sm sticky-top">
+      <div className="container d-flex justify-content-between align-items-center">
         <Link className="navbar-brand fw-bold" to="/">
           WeiCook
         </Link>
 
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/menu">
-                Menu
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/edit">
-                Edit
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="btn btn-primary ms-lg-3" to="/login">
-                Login
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-nav flex-row gap-3">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/menu">
+              Menu
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/edit">
+              Edit
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="btn btn-primary ms-lg-3" to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
